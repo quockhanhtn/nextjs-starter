dfa329423288317d0ea3a26a36003c133837d39b
Khanh Lam <quockhanhdev@gmail.com>
2023-07-01 21:08:50 +0700

    Update Eslint config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  pageExtensions: ['ts', 'tsx'],
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
