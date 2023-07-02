034514e2309a8af3fa8de5529336107c33f65c8a
Khanh Lam <quockhanhdev@gmail.com>
2023-07-02 18:23:17 +0700

    Add example pages

---
+++ src/components/example/button-as-child.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import { Button } from '~/components/ui/button';
+
+export default function ButtonAsChild() {
+  return (
+    <Button asChild>
+      <Link href="/login">Login</Link>
+    </Button>
+  );
+}
