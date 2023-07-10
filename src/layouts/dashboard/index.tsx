5e6c063e402a96a48639b01c518e467381fb6ae7
Khanh Lam <quockhanhdev@gmail.com>
2023-07-10 23:05:26 +0700

    Add hook useMediaQuery

--- src/layouts/dashboard/index.tsx
+++ src/layouts/dashboard/index.tsx
@@ -1,17 +1,15 @@
+'use client';
+
+import { useMediaQuery } from '~/hooks';
 import DashboardSidebar from '~/layouts/dashboard/DashboardSidebar';
 
-export type DashboardLayoutProps = {
-  children: React.ReactNode;
-};
-
-const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+const DashboardLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const isMobile = useMediaQuery('(max-width: 768px)');
   return (
-    <div className="bg-background">
-      <div className="grid lg:grid-cols-5">
-        <DashboardSidebar className="hidden lg:block" />
-        <div className="col-span-3 lg:col-span-4 lg:border-l">
-          <div className="h-full px-4 py-6 lg:px-8">{children}</div>
-        </div>
+    <div className="bg-background w-screen h-screen flex flex-row">
+      <DashboardSidebar className={isMobile ? 'hidden' : `w-[200px] block`} />
+      <div className="flex-grow">
+        <div className="h-full px-4 py-6 lg:px-8">{children}</div>
       </div>
     </div>
   );
